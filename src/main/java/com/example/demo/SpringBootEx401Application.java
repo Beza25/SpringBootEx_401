2327c630601db77974cc093b08723c3571e1e394
GBTC405016UR <GBTC405016UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-08-21 18:40:08 -0400

    SpringBootEx_401

---
+++ src/main/java/com/example/demo/SpringBootEx401Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEx401Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootEx401Application.class, args);
+    }
+
+}
